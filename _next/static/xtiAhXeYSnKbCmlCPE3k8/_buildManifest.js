128f30bceeebc748dc9dd150b41ac9a95b1fd019
Sahil <sahilsingh90@gmail.com>
2023-01-11 19:19:48 +0000

    review

---
+++ _next/static/xtiAhXeYSnKbCmlCPE3k8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/173-9d75fd3417f1cc16.js","static/chunks/pages/index-6533574ab893f3a7.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
