24ae1f142d9f9e5ce77a0b06fbf838e123234411
Sahil <sahilsingh90@gmail.com>
2023-01-11 18:17:15 +0000

    Revision

---
+++ _next/static/Dxt9kW-eMCeE5VjAA7CmG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/399-83a17d2cff27598e.js","static/chunks/pages/index-aa00483f9ee9b66c.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
