b7ba1e84290c7709491f5fa6eeb1c03114318012
Sahil <sahilsingh90@gmail.com>
2023-01-11 18:45:15 +0000

    update

---
+++ _next/static/anrYVjI-_MPK6DGHDCK-f/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/76-57651ee884738ac2.js","static/chunks/pages/index-ef4fb28c7d14ba38.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
